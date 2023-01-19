066874b24984320aebdd3bfebb6350f26342b784
Nico Perez <nicolasperez@multiplica.com>
2023-01-19 19:07:30 -0300

    feat - home php version

---
+++ html/v-home.php
@@ -0,0 +1,320 @@
+<?php 
+    /* Template Name: Landing */ 
+    $URL = get_stylesheet_directory_uri();
+?>
+
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Optimiza Consulting"/>
+    <meta name="description" content=""/>
+    <meta property="og:title" content="Optimiza Consulting"/>
+    <meta property="og:description" content="Transforma tus costos de Recursos Humanos en oportunidades de ingresos y ahorrosOptimiza"/>
+
+    <meta property="og:type" content="company"/>
+    <meta property="og:url" content="https://optimizaconsulting.cl/"/>
+    <meta property="og:site_name" content="Optimiza Consulting"/>
+    <title>Optimiza Consulting</title>
+    <link rel="shortcut icon" href="<?php echo $URL; ?>/assets/img/favicon.png"/>
+    <link rel="stylesheet" href="<?php echo $URL; ?>/assets/css/style.css">
+</head>
+
+<body>
+    <div class="header header--home">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 col-sm-3 col-4">
+                    <a href="https://web.talana.com/" class="header__logo" target="_blank">
+                        <img src="<?php echo $URL; ?>/assets/img/talana-logo.png" alt="Talana">
+                    </a>
+                </div>
+                <div class="offset-lg-8 offset-sm-6 offset-4 col-lg-2 col-sm-3 col-4">
+                    <a href="https://optimizaconsulting.cl/" class="header__logo">
+                        <img src="<?php echo $URL; ?>/assets/img/optimiza-logo.png" alt="Optimiza">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bgblock bgblock--top bgblock--pbottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="u-center u-relative">
+                        <h1 class="text text--bold text-5xl u-mb20">
+                            Transforma tus costos de recursos humanos en oportunidades de ingresos y ahorros
+                        </h1>
+                        <h4 class="text text-xl text-ls10 u-mb30">
+                            OPTIMIZA CONSULTING
+                        </h4>
+                        <button class="btn btn--fifth text text--bold" data-tf-popup="dY2T03Xh" data-tf-opacity="100"
+                        data-tf-size="100" data-tf-iframe-props="title=Beneficios Talana & Optimiza" data-tf-transitive-search-params>Contáctanos</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="svg-container">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120">
+                <defs>
+                    <linearGradient id="hgradient">
+                        <stop offset="5%" stop-color="#584a94" />
+                        <stop offset="95%" stop-color="#584a94" />
+                    </linearGradient>
+                </defs>
+                <path d="M0,32L80,42.7C160,53,320,75,480,80C640,85,800,75,960,64C1120,53,1280,43,1360,37.3L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+            </svg>
+        </div>
+    </div>
+
+    <div class="bgneutro features u-pb30">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="u-center u-mt60 u-mb90">
+                        <h2 class="text text-3xl">
+                            Aclaramos sus deudas previsionales, conseguimos nuevos ingresos económicos a través de la gestión de subsidios a la contratación y recuperación de pagos previsionales en exceso. 
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5 offset-md-1">
+                    <h2 class="text text-3xl u-center">
+                        Servicios:
+                    </h2>
+                </div>
+                <div class="col-md-5">
+                    <ul>
+                        <li>
+                            Regularización de deuda previsional
+                        </li>
+                        <li>
+                            Recuperación de pagos en exceso
+                        </li>
+                        <li>
+                            Subsidios a la Contratación
+                        </li>
+                    </ul>
+                    <button class="btn btn--secondary text text--bold" data-tf-popup="dY2T03Xh" data-tf-opacity="100"
+                    data-tf-size="100" data-tf-iframe-props="title=Beneficios Talana & Optimiza" data-tf-transitive-search-params>Contáctanos</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bgneutro u-pt40 u-pb40">
+        <div class="container u-pb80 u-pt80">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="slider">
+                        <div class="u-center u-mb70">
+                            <h2 class="text text-4xl text---semibold">
+                                Nuestros clientes
+                            </h2>
+                            <h3 class="text text-xl">
+                                Han confiando en nosotros
+                            </h3>
+                        </div>
+                        <div class="slider__brands">
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-finis.jpg" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-iss.jpg" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-komatsu.jpg" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-sigdo.jpg" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-tecno.jpg" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-uss.jpg" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-ahumada.png" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-breden.png" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-ebco.png" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-knop.png" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-progestion.png" alt="Logo">
+                            </div>
+                            <div class="slider__icons">
+                                <img src="<?php echo $URL; ?>/assets/img/brand-tawa.png" alt="Logo">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bgpurple bars__wrapper u-pt90">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="bars__title u-center">
+                        <h2 class="text text--bold text-5xl u-mb20">
+                            Beneficios
+                        </h2>
+                    </div>
+                    <div class="bars">
+                        <div class="bars__item bars__item--yellow">
+                            <div class="bars__icon"></div>
+                            <div class="bars__column transition">
+                                <div class="bars__column__head">
+                                    <div class="bars__column__number">
+                                        1
+                                    </div>
+                                </div>
+                                <div class="bars__column__text">
+                                    Generación de ingresos y ahorros económicos
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bars__item bars__item--orange">
+                            <div class="bars__icon"></div>
+                            <div class="bars__column transition">
+                                <div class="bars__column__head">
+                                    <div class="bars__column__number">
+                                        2
+                                    </div>
+                                </div>
+                                <div class="bars__column__text">
+                                    Sin costos fijos, cobramos un porcentaje de los ingresos y ahorros generados
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bars__item bars__item--red">
+                            <div class="bars__icon"></div>
+                            <div class="bars__column transition">
+                                <div class="bars__column__head">
+                                    <div class="bars__column__number">
+                                        3
+                                    </div>
+                                </div>
+                                <div class="bars__column__text">
+                                    Todas las gestiones son realizadas por nuestro equipo
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bars__item bars__item--sky">
+                            <div class="bars__icon"></div>
+                            <div class="bars__column transition">
+                                <div class="bars__column__head">
+                                    <div class="bars__column__number">
+                                        4
+                                    </div>
+                                </div>
+                                <div class="bars__column__text">
+                                    Compartimos nuestro conocimiento
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bars__item bars__item--violet">
+                            <div class="bars__icon"></div>
+                            <div class="bars__column transition">
+                                <div class="bars__column__head">
+                                    <div class="bars__column__number">
+                                        5
+                                    </div>
+                                </div>
+                                <div class="bars__column__text">
+                                    Asesoría y visión externa de un equipo con amplia experiencia en recursos humanos
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bars__item bars__item--emerald">
+                            <div class="bars__icon"></div>
+                            <div class="bars__column transition">
+                                <div class="bars__column__head">
+                                    <div class="bars__column__number">
+                                        6
+                                    </div>
+                                </div>
+                                <div class="bars__column__text">
+                                    Resultados a corto plazo
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="bgpale videoplayer u-pt100">
+        <div class="container u-pb30 u-pt30">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="u-mb50 u-center">
+                        <button class="btn btn--secondary text text--bold" data-tf-popup="dY2T03Xh" data-tf-opacity="100"
+                        data-tf-size="100" data-tf-iframe-props="title=Beneficios Talana & Optimiza" data-tf-transitive-search-params>Contáctanos</button>
+                    </div>
+                    <div class="u-center u-mb100">
+                        <h2 class="text text-3xl text---semibold u-mb40">
+                            Optimiza en 2 minutos
+                        </h2>
+                         <iframe src="https://www.youtube.com/embed/rqYLQ7oDlcQ">
+                        </iframe> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="svg-container">
+            <svg viewBox="0 0 1440 320" version="1.1" xmlns="http://www.w3.org/2000/svg" class="svg-content">
+                <defs>
+                    <linearGradient id="fgradient">
+                        <stop offset="5%" stop-color="#9c72b9" />
+                        <stop offset="95%" stop-color="#3e4ba7" />
+                    </linearGradient>
+                </defs>
+                  
+                <path d="M0,0L26.7,10.7C53.3,21,107,43,160,53.3C213.3,64,267,64,320,101.3C373.3,139,427,213,480,208C533.3,203,587,117,640,106.7C693.3,96,747,160,800,186.7C853.3,213,907,203,960,202.7C1013.3,203,1067,213,1120,218.7C1173.3,224,1227,224,1280,218.7C1333.3,213,1387,203,1413,197.3L1440,192L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
+            </svg>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <ul>
+                        <li class="text text-xl">
+                            Síguenos en
+                        </li>
+                        <li class="footer__social">
+                            <a class="footer__social__ln" href="https://www.linkedin.com/company/optimiza-consulting-chile/" target="_blank"></a>
+                        </li>
+                        <li class="footer__social">
+                            <a class="footer__social__ml" href="https://optimizaconsulting.cl/contacto/" target="_blank"></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 footer__rights u-right">
+                    <p class="text text-xl">
+                        &copy; All rights reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="<?php echo $URL; ?>/assets/js/all.js"></script>
+    <script src="//embed.typeform.com/next/embed.js"></script>
+</body>
+
+</html>
